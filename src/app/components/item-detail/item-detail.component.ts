071875ca3819fec50d12851834e9f012a83d61f1
Angel <48069264+velk20@users.noreply.github.com>
2024-03-10 20:40:50 +0200

    delete item

--- src/app/components/item-detail/item-detail.component.ts
+++ src/app/components/item-detail/item-detail.component.ts
@@ -1,43 +1,49 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute} from "@angular/router";
-import {FurnitureService} from "../../services/furniture.service";
-import {Furniture} from "../../model/furniture";
-import {LocalStorageService} from "../../services/local-storage.service";
-import {UserService} from "../../services/user.service";
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { FurnitureService } from '../../services/furniture.service';
+import { Furniture } from '../../model/furniture';
+import { LocalStorageService } from '../../services/local-storage.service';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-item-detail',
   templateUrl: './item-detail.component.html',
-  styleUrls: ['./item-detail.component.css']
+  styleUrls: ['./item-detail.component.css'],
 })
-export class ItemDetailComponent implements OnInit{
+export class ItemDetailComponent implements OnInit {
   item: Furniture = {} as Furniture;
-  constructor(private activatedRoute:ActivatedRoute,
-              private furnitureService:FurnitureService,
-              private localStorageService:LocalStorageService,
-              private userService:UserService) {
-  }
+
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private furnitureService: FurnitureService,
+    private localStorageService: LocalStorageService,
+    private userService: UserService,
+  ) {}
 
   ngOnInit(): void {
-    this.activatedRoute.params.subscribe((params)=>{
+    this.activatedRoute.params.subscribe((params) => {
       const itemId = params['id'];
-      this.furnitureService.getOne(itemId).subscribe(item=>{
+      this.furnitureService.getOne(itemId).subscribe((item) => {
         this.item = item;
-      })
-    })
-    }
-
-
-  deleteItem(id: number) {
-    alert("Are you sure you want to delete this item?")
+      });
+    });
   }
 
-  isOwnerOrAdmin():boolean {
+  deleteItem(id: number) {
+    if (confirm('Are you sure you want to delete this item?')) {
+      this.furnitureService.delete(id).subscribe(() => {
+        alert('You successfully delete this item!');
+        this.router.navigate(['/dashboard']);
+      });
+    }
+  }
+
+  isOwnerOrAdmin(): boolean {
     let userId = this.localStorageService.getCurrentUserIdFromJwt();
-    if (userId){
-      return (this.item.userId === userId);
-    }else {
+    if (userId) {
+      return this.item.userId === userId;
+    } else {
       return false;
     }
   }
--- src/app/services/furniture.service.ts
+++ src/app/services/furniture.service.ts
@@ -1,17 +1,16 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Constant} from "./constant";
-import {Observable} from "rxjs";
-import {CreateFurniture, Furniture} from "../model/furniture";
+import { HttpClient } from '@angular/common/http';
+import { Constant } from './constant';
+import { Observable } from 'rxjs';
+import { CreateFurniture, Furniture } from '../model/furniture';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class FurnitureService {
   furnitureUrl = Constant.BASE_URL + '/furniture';
 
-  constructor(private http: HttpClient) {
-  }
+  constructor(private http: HttpClient) {}
 
   getOne(id: number): Observable<Furniture> {
     return this.http.get<Furniture>(`${this.furnitureUrl}/${id}`);
@@ -27,5 +26,9 @@ export class FurnitureService {
 
   update(id: number, furniture: Furniture): Observable<Furniture> {
     return this.http.put<Furniture>(`${this.furnitureUrl}/${id}`, furniture);
+  }
+
+  delete(id: number) {
+    return this.http.delete(`${this.furnitureUrl}/${id}`);
   }
 }
