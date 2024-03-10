99b278ba790f5d137609ff7c97a5370cd43918b2
Angel <48069264+velk20@users.noreply.github.com>
2024-03-10 20:34:02 +0200

    Update profile without password

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,27 +1,23 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {User, UserLogin, UserRegister, UserUpdate} from "../model/user";
-import {Constant} from "./constant";
-import {JwtToken} from "../model/jwt";
-import {Observable} from "rxjs";
-import {bodyParsingHandler} from "json-server-auth/dist/shared-middlewares";
+import { HttpClient } from '@angular/common/http';
+import { User, UserLogin, UserRegister, UserUpdate } from '../model/user';
+import { Constant } from './constant';
+import { JwtToken } from '../model/jwt';
+import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class UserService {
-
   userUrl = Constant.BASE_URL + '/users';
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient) {}
 
-  }
-
-  register(user:UserRegister): Observable<JwtToken> {
+  register(user: UserRegister): Observable<JwtToken> {
     return this.http.post<JwtToken>(Constant.BASE_URL + '/register', user);
   }
 
-  login(user: UserLogin):Observable<JwtToken> {
+  login(user: UserLogin): Observable<JwtToken> {
     return this.http.post<JwtToken>(Constant.BASE_URL + '/login', user);
   }
 
@@ -33,16 +29,15 @@ export class UserService {
     return this.http.get<User>(`${this.userUrl}/${id}`);
   }
 
-  update(id: number, user:UserUpdate): Observable<User> {
-    return this.http.put<User>(`${this.userUrl}/${id}`, user);
+  update(id: number, user: UserUpdate): Observable<User> {
+    return this.http.patch<User>(`${this.userUrl}/${id}`, user);
   }
 
   delete(id: number): Observable<{}> {
     return this.http.delete<{}>(`${this.userUrl}/${id}`);
   }
 
-  getByEmail(email: string):Observable<User[]> {
+  getByEmail(email: string): Observable<User[]> {
     return this.http.get<User[]>(`${this.userUrl}?email like=${email}`);
   }
-
 }
