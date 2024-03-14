d046784849f1ae7be338ac8d84ed1b32cc3d962a
Angel Mladenov <a.mladenov@seeburger.com>
2024-03-14 14:50:25 +0200

    Sweet Alerts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,34 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {NgOptimizedImage} from "@angular/common";
-import {RegisterComponent} from './components/register/register.component';
-import {HTTP_INTERCEPTORS, HttpClientModule} from "@angular/common/http";
-import {MyInterceptor} from "./auth/jwt-interceptor";
-import {HeaderComponent} from "./components/header/header.component";
-import {HomeComponent} from "./components/home/home.component";
-import {DashboardComponent} from "./components/dashboard/dashboard.component";
-import {FooterComponent} from "./components/footer/footer.component";
-import {LoginComponent} from "./components/login/login.component";
-import {ErrorComponent} from './components/error/error.component';
-import {ReactiveFormsModule} from "@angular/forms";
-import {ItemComponent} from './components/item/item.component';
-import {AddItemComponent} from "./components/add-item/add-item.component";
-import {AboutComponent} from './components/about/about.component';
-import {ItemDetailComponent} from './components/item-detail/item-detail.component';
-import {ItemEditComponent} from './components/item-edit/item-edit.component';
-import {ProfileComponent} from './components/profile/profile.component';
-import {ProfileChangePasswordComponent} from './components/profile-change-password/profile-change-password.component';
-import {MyItemsComponent} from './components/my-items/my-items.component';
-import {AddCategoryComponent} from './components/add-category/add-category.component';
-import {ToastrModule} from "ngx-toastr";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NgOptimizedImage } from '@angular/common';
+import { RegisterComponent } from './components/register/register.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { MyInterceptor } from './auth/jwt-interceptor';
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { LoginComponent } from './components/login/login.component';
+import { ErrorComponent } from './components/error/error.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ItemComponent } from './components/item/item.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { AboutComponent } from './components/about/about.component';
+import { ItemDetailComponent } from './components/item-detail/item-detail.component';
+import { ItemEditComponent } from './components/item-edit/item-edit.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { ProfileChangePasswordComponent } from './components/profile-change-password/profile-change-password.component';
+import { MyItemsComponent } from './components/my-items/my-items.component';
+import { AddCategoryComponent } from './components/add-category/add-category.component';
+import { ToastrModule } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { UserListComponent } from './components/user-list/user-list.component';
 import { CategoryListComponent } from './components/category-list/category-list.component';
 import { CategoryEditComponent } from './components/category-edit/category-edit.component';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
 
 @NgModule({
   declarations: [
@@ -50,7 +51,7 @@ import { CategoryEditComponent } from './components/category-edit/category-edit.
     AddCategoryComponent,
     UserListComponent,
     CategoryListComponent,
-    CategoryEditComponent
+    CategoryEditComponent,
   ],
   imports: [
     BrowserModule,
@@ -59,12 +60,12 @@ import { CategoryEditComponent } from './components/category-edit/category-edit.
     ReactiveFormsModule,
     AppRoutingModule,
     BrowserAnimationsModule, // required animations module
+    SweetAlert2Module.forRoot(),
     ToastrModule.forRoot(),
   ],
   providers: [
-    {provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true},
+    { provide: HTTP_INTERCEPTORS, useClass: MyInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
