8c286dbd7dc0f7176086b4e8d5587fbfddb6aa84
Angel Mladenov <a.mladenov@seeburger.com>
2024-03-25 09:05:44 +0200

    optimazing

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,16 +1,9 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import {
-  IsAdminUser,
-  UpdateLikedItems,
-  User,
-  UserLogin,
-  UserRegister,
-  UserUpdate,
-} from '../model/user';
-import { Constant } from './constant';
-import { JwtTokenWithUser } from '../model/jwt';
-import { map, Observable, Subject } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {IsAdminUser, UpdateLikedItems, User, UserLogin, UserRegister, UserUpdate,} from '../model/user';
+import {Constant} from './constant';
+import {JwtTokenWithUser} from '../model/jwt';
+import {map, Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -18,7 +11,8 @@ import { map, Observable, Subject } from 'rxjs';
 export class UserService {
   userUrl = Constant.BASE_URL + '/users';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+  }
 
   register(user: UserRegister): Observable<JwtTokenWithUser> {
     return this.http.post<JwtTokenWithUser>(
@@ -58,23 +52,6 @@ export class UserService {
 
   delete(id: number) {
     return this.http.delete(`${this.userUrl}/${id}?_dependent=furniture`);
-  }
-
-  isUserAdmin(id: number): Observable<boolean> {
-    const result = new Subject<boolean>();
-
-    this.getOne(id).subscribe(
-      (user) => {
-        result.next(user.isAdmin);
-        result.complete();
-      },
-      (error) => {
-        result.next(false);
-        result.complete();
-      },
-    );
-
-    return result.asObservable();
   }
 
   getByEmail(email: string): Observable<User[]> {
